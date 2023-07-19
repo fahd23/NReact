05e0058bd8d36df2dea6259b93e05279adea2964
Fahd <64308102+fahd23@users.noreply.github.com>
2023-07-19 10:24:36 +0530

    Commit: Ep-01 With parcel

--- App.js
+++ App.js
@@ -1,7 +1,10 @@
 const heading = React.createElement(
-  "h1",
-  { id: "heading" },
-  "Hello world from react!"
+  "div",
+  { id: "parent" },
+  React.createElement("div", { id: "children" }, [
+    React.createElement("h1", { id: "heading" }, "Hello world from react!"),
+    React.createElement("h2", { id: "heading2" }, "Hello world from react!"),
+  ])
 );
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
