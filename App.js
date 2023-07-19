611f1db9c63d775f530cbde585b9ecd12299eaee
Fahd <64308102+fahd23@users.noreply.github.com>
2023-07-19 17:41:01 +0530

    Episode - 02

--- App.js
+++ App.js
@@ -1,9 +1,20 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+
 const heading = React.createElement(
   "div",
   { id: "parent" },
   React.createElement("div", { id: "children" }, [
-    React.createElement("h1", { id: "heading" }, "Hello world from react!"),
-    React.createElement("h2", { id: "heading2" }, "Hello world from react!"),
+    React.createElement(
+      "h1",
+      { id: "heading", key: "1" },
+      "Hello world from react!"
+    ),
+    React.createElement(
+      "h2",
+      { id: "heading2", key: "2" },
+      "Hello world from react!"
+    ),
   ])
 );
 
--- README.md
+++ README.md
@@ -1,1 +1,19 @@
 # React Fundamentals
+
+# Parcel
+
+- Dev Build
+- Local server
+- HMR - Hot Module Replacement
+- File Watching Algorithm -written in C++
+- Cache - Faster builds
+- Image Optimization
+- Minification
+- Bundling
+- Compress
+- Diagnostics
+- Code Splitting
+- Error Handling
+- HTTPs
+- Differential Bundling - support older bowser
+- Tree Shaking
