7739dff32ebdc9eb110ca302236b2c9089d41bf7
Fahd <64308102+fahd23@users.noreply.github.com>
2023-07-24 18:40:20 +0530

    Episode - 03

--- App.js
+++ App.js
@@ -1,23 +1,15 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 
-const heading = React.createElement(
-  "div",
-  { id: "parent" },
-  React.createElement("div", { id: "children" }, [
-    React.createElement(
-      "h1",
-      { id: "heading", key: "1" },
-      "Hello world from react!"
-    ),
-    React.createElement(
-      "h2",
-      { id: "heading2", key: "2" },
-      "Hello world from react!"
-    ),
-  ])
-);
+const App = () => {
+  return (
+    <>
+      <h1 id="heading">Hello from Jsx</h1>
+      <p>Para</p>
+    </>
+  );
+};
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 
-root.render(heading);
+root.render(<App />);
